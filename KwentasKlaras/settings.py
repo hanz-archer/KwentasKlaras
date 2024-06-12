a405713f2a84c4437a11527b8590f3b771a79d37
Hanz <dalubatanhans@gmaal.com>
2024-06-12 23:03:24 +0800

    collecstatic

--- KwentasKlaras/settings.py
+++ KwentasKlaras/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'KwentasApp/static'),
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = 	'/home/KwentasKlarasPMIS/KwentasKlaras/KwentasApp/static'
 
 
 
